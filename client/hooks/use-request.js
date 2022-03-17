7805eead81a821440e330617c4db1581dab1ff6d
realmers <danhoalexander123@gmail.com>
2022-03-17 18:38:31 +0100

    hooks, implement signin, signout and more

---
+++ client/hooks/use-request.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default ({ url, method, body, onSuccess }) => {
+	const [errors, setErrors] = useState(null);
+
+	const doRequest = async () => {
+		try {
+			data = { ...data, ...props };
+
+			const config = {
+				method,
+				url,
+				data,
+			};
+
+			const response = await axios(config);
+			if (onSuccess) {
+				onSuccess(response.data);
+			}
+			return response.data;
+		} catch (err) {
+			setErrors(
+				<div className="alert alert-danger">
+					<h4>Ooops....</h4>
+					<ul className="my-0">
+						{err.response.data.errors.map((err) => (
+							<li key={err.message}>{err.message}</li>
+						))}
+					</ul>
+				</div>
+			);
+		}
+	};
+
+	return { doRequest, errors };
+};
