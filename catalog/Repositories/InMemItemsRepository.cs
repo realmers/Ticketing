7e425318f087de13e1e708e8479f8adf9d77f2ce
realmers <danhoalexander123@gmail.com>
2023-01-18 16:41:07 +0100

    change in sample items

--- catalog/Repositories/InMemItemsRepository.cs
+++ catalog/Repositories/InMemItemsRepository.cs
@@ -9,9 +9,9 @@ namespace Catalog.Repositories
     {
         private readonly List<Item> items = new()
         {
-            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
-            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
-            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
+            new Item { Id = Guid.NewGuid(), Name = "Ticket 1", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Ticket 2", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Ticket 3", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
         };
 
         public IEnumerable<Item> GetItems()
